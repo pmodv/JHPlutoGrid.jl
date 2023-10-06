5dc799e3a4438eb663545985ad95e2a59602d8c4
pvernes <phil.vernes@juliahub.com>
2023-10-06 18:08:14 -0400

    token

--- src/PlutoGrid.jl
+++ src/PlutoGrid.jl
@@ -11,8 +11,7 @@ const MODIFIED_COL_REGEX = r"^(?!modified_column_).+"
 
 const AG_GRID_VERSION = "30.1.0"
 
-agGrid.LicenseManager.setLicenseKey("[TRIAL]_this_AG_Grid_Enterprise_key_( AG-047238 )_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_( legal@ag-grid.com )___For_help_with_purchasing_a_production_key_please_contact_( info@ag-grid.com )___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_( 31 October 2023 )____[v2]_MTY5ODcxMDQwMDAwMA==79d599f48a9d39e35170430e849f6741
-");
+agGrid.LicenseManager.setLicenseKey("");
 
 
 # to-do - add header for aggregation function (custom)
